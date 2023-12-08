37eb4b40f39007a46d28d91db889752c7b618d32
Tom Fahy <thomas.fahy9@gmail.com>
2023-12-08 19:40:34 +0000

    add textareas to enter events to HTML

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,31 @@
 //DOM elements
 let currentDayEl = $('#currentDay');
 let timeblockEls = $(".row");
+let textboxEls = $("textarea");
 
 //get today and put it at the top of the page
 let today = dayjs();
 currentDayEl.text(today.format("dddd, MMMM D"));
 
-let currentHour = today.format("H");
-let websiteFormat =  today.format("hA");
+let currentHour = dayjs().hour();
 
 // loop through all rows and color according to time.
 timeblockEls.each(function() {
-    if ($(this).children().eq(0).text() == "9AM") {
-        $(this).children().eq(1).addClass("present")
+    if ($(this).children().eq(0).data("hour") < currentHour) {
+        $(this).children().eq(1).addClass("past");
+    } else if ($(this).children().eq(0).data("hour") == currentHour) {
+        $(this).children().eq(1).addClass("present");
+    } else {
+        $(this).children().eq(1).addClass("future");
     }
 });
+
+
+//function to store the text
+function saveText() {
+
+}
+
+//onclick thing
 
 
