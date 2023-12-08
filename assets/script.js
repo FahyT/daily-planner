bb257137df6273b7f4f1612f435e6090eb210de6
Tom Fahy <thomas.fahy9@gmail.com>
2023-12-08 19:06:04 +0000

    add data-hour attributes to HTML

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,15 @@
 //DOM elements
 let currentDayEl = $('#currentDay');
+let timeblockEls = $(".row");
 
 //get today and put it at the top of the page
 let today = dayjs();
 currentDayEl.text(today.format("dddd, MMMM D"));
 
-//get time of block and color accordingly
+let currentHour = today.format("H");
+let websiteFormat =  today.format("hA");
 
-let timeblockEls = $(".row");
-
+// loop through all rows and color according to time.
 timeblockEls.each(function() {
     if ($(this).children().eq(0).text() == "9AM") {
         $(this).children().eq(1).addClass("present")
